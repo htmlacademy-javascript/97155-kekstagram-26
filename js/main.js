a27b40cd8b25ce980759edae7cb49e1d328a07ec
Михаил Воронков <redvoronik@yandex.ru>
2022-06-10 17:06:03 +0300

    Упростил работу функций

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Возвращает рандомное число из указанного интервала включительно
 function getRandomInt(min, max) {
   if (min > max) {
-    min = Math.floor(min);
-    max = Math.ceil(max);
-    return Math.floor(Math.random() * (min - max + 1)) + max;
+    const temp = min;
+    min = max;
+    max = temp;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -12,11 +12,12 @@ function getRandomInt(min, max) {
 
 // Проверяет умещается ли строка в указанный лимит символов
 function checkStringLength(string, limit) {
-  if (string.length > limit) {
-    return false;
-  }
-  return true;
+  return string.length <= limit;
+  // if (string.length > limit) {
+  //   return false;
+  // }
+  // return true;
 }
 
-getRandomInt(10, 10);
+getRandomInt(10, 1);
 checkStringLength('Проверка длины строки', 21);
