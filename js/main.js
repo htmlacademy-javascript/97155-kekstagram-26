b8621e036f115eee7f1a913c7bcdc22f391abd63
Михаил Воронков <redvoronik@yandex.ru>
2022-07-27 14:56:11 +0300

    Закончил получение и отправку данных с сервера

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 import { renderPictures } from './picture.js';
 import { renderBigPicture } from './big-picture.js';
+import { closeUploadModal, setUploadFormSubmit } from './image-upload.js';
+import { showAlert } from './util.js';
+import { getData } from './api.js';
 import './image-upload.js';
 import './image-upload-scale.js';
 import './image-upload-filters.js';
 
+// получаем данные о картинках с сервера
+getData((images) => {
+  renderPictures(images);
+  renderBigPicture(images);
+}, showAlert);
 
-fetch('https://26.javascript.pages.academy/kekstagram/data')
-  .then((response) => response.json())
-  .then((images) => {
-    renderPictures(images);
-    renderBigPicture(images);
-  });
+setUploadFormSubmit(closeUploadModal);
